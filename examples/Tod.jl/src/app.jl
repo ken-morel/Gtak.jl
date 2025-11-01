5e55cba4a966264f2723ffc76dc0321e99b96a80
ken-morel <engonken8@gmail.com>
2025-11-01 19:41:41 +0100

    Cant find better docs for menus

--- examples/Tod.jl/src/app.jl
+++ examples/Tod.jl/src/app.jl
@@ -1,4 +1,32 @@
-createapplication() = application("com.example.tod", stylesheet=todstyle) do app
+function on_new_clicked(app)
+    return println("New menu item clicked!")
+end
+
+function on_open_clicked(app)
+    return println("Open menu item clicked!")
+end
+
+function on_toggle_setting(app)
+    return println("Toggle setting menu item clicked!")
+end
+
+createapplication() = application(
+    "com.example.tod", stylesheet = todstyle, menubar = Menu(
+        "File" => [
+            "New" => on_new_clicked,
+            "Open" => on_open_clicked,
+            "" => nothing,
+            "Exit" => (app) -> Gtk4.quit(app),
+        ],
+        "Settings" => [
+            "Toggle Feature" => Reactant(false),
+            "Theme" => [
+                "Light" => (app) -> println("Set theme to Light"),
+                "Dark" => (app) -> println("Set theme to Dark"),
+            ],
+        ]
+    )
+) do app
     setupstores!(app)
     app.data[:user] = nothing
     window(app; title = "Gtak Todolist") do _
